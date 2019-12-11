8106b6e5810da4f8932cdd8bdf95d759529b6101
Christian Blavier <cblavier@gmail.com>
2019-12-11 11:14:36 +0100

    code style

--- lib/2019/day11/day11.ex
+++ lib/2019/day11/day11.ex
@@ -2,39 +2,41 @@ defmodule Advent.Y2019.Day11 do
   alias Advent.Y2019.Computer
 
   def run(puzzle, initial_color) do
-    program = Computer.parse_program(puzzle)
-    robot = spawn_robot(program, initial_color)
-
     hull =
-      [:color, :direction]
-      |> Stream.cycle()
-      |> Enum.reduce_while({%{}, {0, 0}, :up}, fn instruction, {hull, position, direction} ->
-        receive do
-          {:input, input} ->
-            case instruction do
-              :color ->
-                {:cont, {Map.put(hull, position, input), position, direction}}
-
-              :direction ->
-                {new_position, new_direction} = direction(position, direction, input)
-                send(robot, {:input, Map.get(hull, new_position, 0)})
-                {:cont, {hull, new_position, new_direction}}
-            end
-
-          :halt ->
-            {:halt, hull}
-        end
-      end)
+      puzzle
+      |> Computer.parse_program()
+      |> spawn_robot(initial_color, self())
+      |> paint_hull()
 
     {hull, hull |> Map.keys() |> length()}
   end
 
-  defp spawn_robot(program, input) do
-    pid = self()
-
+  defp spawn_robot(program, input, parent_pid) do
     spawn(fn ->
-      Computer.run_program(program, [input], pid)
-      send(pid, :halt)
+      Computer.run_program(program, [input], parent_pid)
+      send(parent_pid, :halt)
+    end)
+  end
+
+  defp paint_hull(robot) do
+    [:color, :direction]
+    |> Stream.cycle()
+    |> Enum.reduce_while({%{}, {0, 0}, :up}, fn instruction, {hull, position, direction} ->
+      receive do
+        {:input, input} ->
+          case instruction do
+            :color ->
+              {:cont, {Map.put(hull, position, input), position, direction}}
+
+            :direction ->
+              {new_position, new_direction} = direction(position, direction, input)
+              send(robot, {:input, Map.get(hull, new_position, 0)})
+              {:cont, {hull, new_position, new_direction}}
+          end
+
+        :halt ->
+          {:halt, hull}
+      end
     end)
   end
 
