4808e2c54f7713a90f08652a58133d530237b066
Christian Blavier <cblavier@gmail.com>
2019-12-13 10:06:08 +0100

    refactoring

--- lib/2019/day12/part1.ex
+++ lib/2019/day12/part1.ex
@@ -1,8 +1,4 @@
 defmodule Advent.Y2019.Day12.Part1 do
-  defmodule Planet do
-    defstruct [:id, :x, :y, :z, vel_x: 0, vel_y: 0, vel_z: 0]
-  end
-
   @doc ~S"""
   iex> alias Advent.Y2019.Day12.Part1
   iex> puzzle = ""
@@ -11,102 +7,75 @@ defmodule Advent.Y2019.Day12.Part1 do
   iex> puzzle = puzzle <> "<x=4, y=-8, z=8>\n"
   iex> puzzle = puzzle <> "<x=3, y=5, z=-1>"
   iex> Part1.run(puzzle, 1)
-  {%{
-    0 => %Advent.Y2019.Day12.Part1.Planet{id: 0, x: 2, y: -1, z: 1,  vel_x: 3,  vel_y: -1, vel_z: -1},
-    1 => %Advent.Y2019.Day12.Part1.Planet{id: 1, x: 3, y: -7, z: -4, vel_x: 1,  vel_y: 3,  vel_z: 3},
-    2 => %Advent.Y2019.Day12.Part1.Planet{id: 2, x: 1, y: -7, z: 5,  vel_x: -3, vel_y: 1,  vel_z: -3},
-    3 => %Advent.Y2019.Day12.Part1.Planet{id: 3, x: 2, y: 2,  z: 0,  vel_x: -1, vel_y: -3, vel_z: 1}
-  }, 229}
+  {[
+    {{2, -1, 1}, {3, -1, -1}},
+    {{3, -7, -4}, {1, 3, 3}},
+    {{1, -7, 5}, {-3, 1, -3}},
+    {{2, 2, 0 }, {-1, -3, 1}}
+  ], 229}
   iex> Part1.run(puzzle, 10)
-  {%{
-    0 => %Advent.Y2019.Day12.Part1.Planet{id: 0, x: 2, y: 1,  z: -3, vel_x: -3,  vel_y: -2, vel_z: 1},
-    1 => %Advent.Y2019.Day12.Part1.Planet{id: 1, x: 1, y: -8, z: 0,  vel_x: -1, vel_y: 1, vel_z: 3},
-    2 => %Advent.Y2019.Day12.Part1.Planet{id: 2, x: 3, y: -6, z: 1,  vel_x: 3,  vel_y: 2, vel_z: -3},
-    3 => %Advent.Y2019.Day12.Part1.Planet{id: 3, x: 2, y: 0,  z: 4,  vel_x: 1, vel_y: -1, vel_z: -1}
-  }, 179}
+  {[
+   {{2, 1, -3}, { -3, -2, 1}},
+   {{1, -8, 0 }, { -1, 1, 3}},
+   {{3, -6, 1 }, { 3, 2, -3}},
+   {{2, 0, 4 }, { 1, -1, -1}}
+  ], 179}
   """
   def run(puzzle, steps) do
-    planets = planet_map(puzzle)
+    planets = puzzle |> String.split("\n") |> Enum.map(&parse_planet/1)
 
     planets =
       1..steps
       |> Enum.reduce(planets, fn _i, planets ->
         planets
-        |> update_velocity([:x, :y, :z])
-        |> apply_movement([:x, :y, :z])
+        |> update_velocity(0..2)
+        |> apply_movement(0..2)
       end)
 
     {planets, compute_energy(planets)}
   end
 
-  def planet_map(puzzle) do
-    puzzle
-    |> String.split("\n")
-    |> Enum.with_index()
-    |> Enum.map(&parse_planet/1)
-    |> Enum.reduce(%{}, fn planet, acc ->
-      Map.put(acc, planet.id, planet)
-    end)
-  end
-
   def update_velocity(planets, axes) do
-    combinations =
-      for p1 <- Map.values(planets),
-          p2 <- Map.values(planets),
-          p1 != p2,
-          do: {p1, p2}
+    Enum.map(planets, fn p1 ->
+      Enum.reduce(planets, p1, fn {pos2, _}, p1 ->
+        Enum.reduce(axes, p1, fn axis, {pos1, velocity} ->
+          accel =
+            cond do
+              elem(pos1, axis) < elem(pos2, axis) -> 1
+              elem(pos1, axis) > elem(pos2, axis) -> -1
+              true -> 0
+            end
 
-    Enum.reduce(combinations, planets, fn {p1, p2}, planets ->
-      Enum.reduce(axes, planets, fn axis, planets ->
-        accel =
-          cond do
-            get_in(p1, [Access.key(axis)]) < get_in(p2, [Access.key(axis)]) -> 1
-            get_in(p1, [Access.key(axis)]) > get_in(p2, [Access.key(axis)]) -> -1
-            true -> 0
-          end
-
-        vel_attr = String.to_atom("vel_#{axis}")
-
-        Map.update!(planets, p1.id, fn planet ->
-          Map.update!(planet, vel_attr, &(&1 + accel))
+          {pos1, put_elem(velocity, axis, elem(velocity, axis) + accel)}
         end)
       end)
     end)
   end
 
   def apply_movement(planets, axes) do
-    planets
-    |> Map.values()
-    |> Enum.reduce(planets, fn p, planets ->
-      Enum.reduce(axes, planets, fn axis, planets ->
-        Map.update!(planets, p.id, fn planet ->
-          velocity = Map.get(planet, String.to_atom("vel_#{axis}"))
-          Map.update!(planet, axis, &(&1 + velocity))
-        end)
+    Enum.map(planets, fn planet ->
+      Enum.reduce(axes, planet, fn axis, {pos, velocity} ->
+        new_pos = put_elem(pos, axis, elem(pos, axis) + elem(velocity, axis))
+        {new_pos, velocity}
       end)
     end)
   end
 
   def compute_energy(planets) do
     planets
-    |> Map.values()
-    |> Enum.map(
-      &((abs(&1.x) + abs(&1.y) + abs(&1.z)) *
-          (abs(&1.vel_x) + abs(&1.vel_y) + abs(&1.vel_z)))
-    )
+    |> Enum.map(fn {{x, y, z}, {vel_x, vel_y, vel_z}} ->
+      (abs(x) + abs(y) + abs(z)) * (abs(vel_x) + abs(vel_y) + abs(vel_z))
+    end)
     |> Enum.sum()
   end
 
-  def parse_planet({planet, i}) do
+  def parse_planet(planet) do
     regex = ~r/<x=(?<x>[-\d]*), *y=(?<y>[-\d]*), *z=(?<z>[-\d]*)>/
+    %{"x" => x, "y" => y, "z" => z} = Regex.named_captures(regex, planet)
 
-    struct(
-      Planet,
-      for(
-        {key, val} <- Regex.named_captures(regex, planet),
-        into: %{id: i},
-        do: {String.to_atom(key), String.to_integer(val)}
-      )
-    )
+    {
+      {String.to_integer(x), String.to_integer(y), String.to_integer(z)},
+      {0, 0, 0}
+    }
   end
 end
--- lib/2019/day12/part2.ex
+++ lib/2019/day12/part2.ex
@@ -2,34 +2,23 @@ defmodule Advent.Y2019.Day12.Part2 do
   alias Advent.Y2019.Day12.Part1
 
   def run(puzzle) do
-    planets = Part1.planet_map(puzzle)
+    planets = puzzle |> String.split("\n") |> Enum.map(&Part1.parse_planet/1)
 
     [period1, period2, period3] =
-      for axis <- [:x, :y, :z] do
-        Task.async(fn ->
-          find_period_for_axis(planets, axis)
-        end)
+      for axis <- 0..2 do
+        Task.async(fn -> find_period_for_axis(planets, planets, axis, 0) end)
       end
       |> Enum.map(&Task.await/1)
 
     lcm(period1, lcm(period2, period3))
   end
 
-  defp find_period_for_axis(planets, axis) do
-    1
-    |> Stream.iterate(&(&1 + 1))
-    |> Enum.reduce_while({planets, %{planets => true}}, fn step, {planets, previous_planets} ->
-      planets =
-        planets
-        |> Part1.update_velocity([axis])
-        |> Part1.apply_movement([axis])
+  defp find_period_for_axis(planets, planets, _axis, step) when step > 0, do: step
 
-      case Map.get(previous_planets, planets) do
-        nil -> {:cont, {planets, Map.put(previous_planets, planets, true)}}
-        _ -> {:halt, step}
-      end
-    end)
+  defp find_period_for_axis(planets, target, axis, step) do
+    next_planets = planets |> Part1.update_velocity([axis]) |> Part1.apply_movement([axis])
+    find_period_for_axis(next_planets, target, axis, step + 1)
   end
 
-  def lcm(a, b), do: round(a * b / Integer.gcd(a, b))
+  def lcm(a, b), do: div(a * b, Integer.gcd(a, b))
 end
