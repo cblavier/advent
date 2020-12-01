2193d801320c5467578924d3be6b6c96f7f47694
Christian Blavier <cblavier@gmail.com>
2020-12-01 10:43:40 +0100

    improved code

--- lib/2020/day01/part1.ex
+++ lib/2020/day01/part1.ex
@@ -14,20 +14,8 @@ defmodule Advent.Y2020.Day01.Part1 do
   iex> find_sum_to([259, 1721, 979, 366, 299, 675, 1456], 2020)
   {1721, 299}
   """
-  def find_sum_to([i | tail], sum) do
-    case find_sum_to(i, tail, sum) do
-      nil -> find_sum_to(tail, sum)
-      {i, j} -> {i, j}
-    end
-  end
-
-  defp find_sum_to(i, list, sum) do
-    Enum.reduce_while(list, nil, fn j, _ ->
-      if i + j == sum do
-        {:halt, {i, j}}
-      else
-        {:cont, nil}
-      end
-    end)
+  def find_sum_to(list, sum) do
+    [result | _] = for i <- list, j <- list, i + j == sum, do: {i, j}
+    result
   end
 end
--- lib/2020/day01/part2.ex
+++ lib/2020/day01/part2.ex
@@ -14,29 +14,8 @@ defmodule Advent.Y2020.Day01.Part2 do
   iex> find_sum_to([259, 1721, 979, 366, 299, 675, 1456], 2020)
   {979, 366, 675}
   """
-  def find_sum_to([i | tail], sum) do
-    case find_sum_to(i, tail, sum) do
-      nil -> find_sum_to(tail, sum)
-      {i, j, k} -> {i, j, k}
-    end
-  end
-
-  def find_sum_to(_i, [], _sum), do: nil
-
-  def find_sum_to(i, [j | tail], sum) do
-    case find_sum_to(i, j, tail, sum) do
-      nil -> find_sum_to(i, tail, sum)
-      {i, j, k} -> {i, j, k}
-    end
-  end
-
-  defp find_sum_to(i, j, list, sum) do
-    Enum.reduce_while(list, nil, fn k, _ ->
-      if i + j + k == sum do
-        {:halt, {i, j, k}}
-      else
-        {:cont, nil}
-      end
-    end)
+  def find_sum_to(list, sum) do
+    [result | _] = for i <- list, j <- list, k <- list, i + j + k == sum, do: {i, j, k}
+    result
   end
 end
