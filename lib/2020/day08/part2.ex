f0e421ef9c425693579d58441a64c13389261939
Christian Blavier <cblavier@gmail.com>
2020-12-08 12:15:42 +0100

    forgot to test nop -> jmp variations

--- lib/2020/day08/part2.ex
+++ lib/2020/day08/part2.ex
@@ -12,7 +12,13 @@ defmodule Advent.Y2020.Day08.Part2 do
     {_, :cycle, visited} = Part1.run_program(program)
 
     visited
-    |> Stream.map(&Map.put(program, &1, {"nop", 0}))
+    |> Stream.map(
+      &Map.update!(program, &1, fn
+        {"nop", count} -> {"jmp", count}
+        {"jmp", _count} -> {"nop", 0}
+        other -> other
+      end)
+    )
     |> Stream.map(&Part1.run_program/1)
     |> Stream.filter(fn
       {_acc, :cycle, _} -> false
