9cb7c6393e9dc2375370bb53d0bf50f32fd55ced
Christian Blavier <cblavier@gmail.com>
2020-12-14 18:55:39 +0100

    💄

--- lib/2020/day14/part2.ex
+++ lib/2020/day14/part2.ex
@@ -1,38 +1,39 @@
 defmodule Advent.Y2020.Day14.Part2 do
+  alias Advent.Y2020.Day14.Part1
+
   def run(puzzle) do
     puzzle
     |> String.split("\n")
     |> Enum.reduce({%{}, nil}, &run_instruction/2)
-    |> Advent.Y2020.Day14.Part1.memory_sum()
+    |> Part1.memory_sum()
   end
 
   def run_instruction(line = "mask" <> _, {memory, _mask}) do
-    {memory, line |> String.split(" = ") |> Enum.at(-1)}
+    {memory, line |> String.split(" = ") |> Enum.at(-1) |> String.graphemes()}
   end
 
   @regex Regex.compile!(~S/mem\[(?<address>\d+)\] = (?<value>\d+)/)
   def run_instruction(instruction, {memory, mask}) do
     %{"address" => address, "value" => value} = Regex.named_captures(@regex, instruction)
-    {address, value} = {String.to_integer(address), String.to_integer(value)}
+
+    address =
+      address
+      |> String.to_integer()
+      |> Integer.to_string(2)
+      |> String.pad_leading(length(mask), "0")
+      |> String.graphemes()
 
     memory =
       address
       |> find_addresses(mask)
       |> Enum.reduce(memory, fn address, memory ->
-        Map.put(memory, address, value)
+        Map.put(memory, address, String.to_integer(value))
       end)
 
     {memory, mask}
   end
 
-  def find_addresses(address, mask) do
-    address
-    |> Integer.to_string(2)
-    |> String.pad_leading(String.length(mask), "0")
-    |> String.graphemes()
-    |> Enum.zip(String.graphemes(mask))
-    |> find_addresses()
-  end
+  def find_addresses(address, mask), do: address |> Enum.zip(mask) |> find_addresses()
 
   def find_addresses(address_and_mask) do
     Enum.reduce(address_and_mask, [0], fn
