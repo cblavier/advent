766f6c3972bfd95e3b5d6bc134d54ed345491c00
Christian Blavier <cblavier@gmail.com>
2020-12-17 18:17:12 +0100

    💄

--- lib/2020/day17/part1.ex
+++ lib/2020/day17/part1.ex
@@ -9,25 +9,17 @@ defmodule Advent.Y2020.Day17.Part1 do
     |> count_cubes()
   end
 
-  def parse_puzzle(puzzle, 3) do
-    y_dim =
-      puzzle
-      |> String.split("\n")
-      |> Enum.with_index()
-      |> Enum.reduce(%{}, fn {row, y}, y_dim ->
-        x_dim =
-          row
-          |> String.graphemes()
-          |> Enum.with_index()
-          |> Enum.reduce(%{}, fn
-            {"#", x}, x_dim -> Map.put(x_dim, x, true)
-            {".", x}, x_dim -> Map.put(x_dim, x, false)
-          end)
-
-        Map.put(y_dim, y, x_dim)
-      end)
-
-    %{0 => y_dim}
+  def parse_puzzle(puzzle, 2) do
+    for {row, y} <- puzzle |> String.split("\n") |> Enum.with_index(), into: %{} do
+      {y,
+       row
+       |> String.graphemes()
+       |> Enum.with_index()
+       |> Enum.reduce(%{}, fn
+         {"#", x}, x_dim -> Map.put(x_dim, x, true)
+         {".", x}, x_dim -> Map.put(x_dim, x, false)
+       end)}
+    end
   end
 
   def parse_puzzle(puzzle, n), do: %{0 => parse_puzzle(puzzle, n - 1)}
@@ -55,27 +47,27 @@ defmodule Advent.Y2020.Day17.Part1 do
   end
 
   def dimension_size(grid, 0) do
-    keys = Map.keys(grid)
-    {Enum.min(keys), Enum.max(keys)}
+    keys = grid |> Map.keys() |> Enum.sort()
+    {Enum.at(keys, 0), Enum.at(keys, -1)}
   end
 
-  def dimension_size(grid, index), do: grid |> Map.get(0) |> dimension_size(index - 1)
+  def dimension_size(grid, i), do: grid |> Map.get(0) |> dimension_size(i - 1)
 
-  def put_in_grid(grid, [index], val), do: Map.put(grid, index, val)
+  def put_in_grid(grid, [i], val), do: Map.put(grid, i, val)
 
-  def put_in_grid(grid, [index | tail], val) do
-    dimension = grid |> Map.get(index, %{}) |> put_in_grid(tail, val)
-    Map.put(grid, index, dimension)
+  def put_in_grid(grid, [i | tail], val) do
+    dimension = grid |> Map.get(i, %{}) |> put_in_grid(tail, val)
+    Map.put(grid, i, dimension)
   end
 
   def cube_next_state(pos, grid) do
     case get_in(grid, pos) do
-      true -> active_neighbours(grid, pos) in 2..3
-      _ -> active_neighbours(grid, pos) == 3
+      true -> active_neighbours_count(grid, pos) in 2..3
+      _ -> active_neighbours_count(grid, pos) == 3
     end
   end
 
-  def active_neighbours(grid, position) do
+  def active_neighbours_count(grid, position) do
     position
     |> neighbours()
     |> Enum.reject(&(&1 == position))
