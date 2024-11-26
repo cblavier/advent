4fb68bb0ae6b7d3e70e4803418dc881bfd1bf858
Christian Blavier <cblavier@gmail.com>
2024-11-26 10:29:10 +0100

    improved day03

--- lib/2017/day03/part1.ex
+++ lib/2017/day03/part1.ex
@@ -2,17 +2,27 @@ defmodule Advent.Y2017.Day03.Part1 do
   def run(puzzle) do
     puzzle = String.to_integer(puzzle)
 
-    n =
-      Stream.iterate(0, &(&1 + 1))
-      |> Stream.filter(fn i -> puzzle <= surface(i) end)
-      |> Enum.at(0)
-
-    surface = surface(n)
-    perimeter = 8 * n
-    singulars = for i <- 0..3, do: surface - n - i * perimeter / 4
-    pos = singulars |> Enum.map(&abs(puzzle - &1)) |> Enum.min()
-    round(pos + n)
+    Stream.iterate({:down, 0}, &next_move/1)
+    |> Stream.flat_map(&flatten_moves/1)
+    |> Enum.reduce_while({1, {0, 0}}, fn move, {index, {x, y}} ->
+      if index == puzzle do
+        {:halt, abs(x) + abs(y)}
+      else
+        new_pos = move({x, y}, move)
+        {:cont, {index + 1, new_pos}}
+      end
+    end)
   end
 
-  defp surface(n), do: :math.pow(2 * n + 1, 2)
+  def next_move({:down, count}), do: {:right, count + 1}
+  def next_move({:right, count}), do: {:up, count}
+  def next_move({:up, count}), do: {:left, count + 1}
+  def next_move({:left, count}), do: {:down, count}
+
+  def flatten_moves({direction, n}), do: List.duplicate(direction, n)
+
+  def move({x, y}, :down), do: {x, y - 1}
+  def move({x, y}, :right), do: {x + 1, y}
+  def move({x, y}, :up), do: {x, y + 1}
+  def move({x, y}, :left), do: {x - 1, y}
 end
